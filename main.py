a2a2f0c8255b48e8e5614b140704e66b9424a287
jai <jaipatel8@outlook.com>
2025-07-09 15:23:08 -0500

    test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from icon_detection import *
 from PnP import *
 from config import *
 
-camera = cv.VideoCapture(1)
+camera = cv.VideoCapture(2)
 
 # Camera Configuration
 camera.set(cv.CAP_PROP_FRAME_WIDTH, 640)
