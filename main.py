73245de8460c67740ff731d8c50173fca35d448d
jai <jaipatel8@outlook.com>
2025-07-09 15:42:47 -0500

    test

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ pipeline = rs.pipeline()
 
 # Configure streams
 config = rs.config()
-config.enable_stream(rs.stream.color, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+#config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
 # Start streaming
 pipeline.start(config)
