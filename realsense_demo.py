00c1b9a7562dc3d59e4b0f0289f7d0f93979d388
jai <jaipatel8@outlook.com>
2025-07-09 18:58:48 -0500

    integrated pf

--- realsense_demo.py
+++ realsense_demo.py
@@ -8,12 +8,25 @@ from typing import Tuple, Optional, List
 from threading import Lock
 from robomaster_particle_filters import fast_plate_orbit
 
+import cv2 as cv
+import numpy as np
+import time
+from frame_proccesing import *
+from armor import *
+from icon_detection import *
+from PnP import *
+from config import *
+import pyrealsense2 as rs
+from draw import *
+
 
 ANGULAR_VELOCITY_STEP_SIZE = 2.0
 POSITION_DIAGONAL_COVARIANCE = np.array([0.0001, 0.0001, 0.0001])
 RADIAL_OFFSETS = [0.0, 0.0, 0.0, 0.0]
 Z_OFFSETS = [0.0, 0.007, 0.0, 0.007]
 VISIBILITY_ANGLE_THRESHOLD = math.pi / 3.0
+
+
 
 
 @dataclass
@@ -156,9 +169,68 @@ CYAN = (255, 255, 0)
 SPACE_KEY = 32
 demo.update_plates([np.array([1,1,0],dtype=np.float64)])
 
+#your mom
+# Create a context object. This object owns the handles to all connected realsense devices
+pipeline = rs.pipeline()
+
+# Configure streams
+config = rs.config()
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 60)
+#config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+# Start streaming
+prof = pipeline.start(config)
+
+
+s = prof.get_device().query_sensors()[1]
+s.set_option(rs.option.exposure, 40)
+
+#main
+
 while True:
+
+    frames = pipeline.wait_for_frames()
+
+    color_frame = frames.get_color_frame()
+
+    frame = np.asanyarray(color_frame.get_data())
+
+    # if not ret:
+    #     print("frame capture fail")
+    #     break
+
+    start_time = time.time()
+    panels = []
+
+    contours = frame_process(frame)
+    lights = bounding_boxes(contours, frame)  # Renamed variable to avoid conflict
+    if len(lights) > 1:
+        try:
+            pairs = pairing(lights)
+            for pair in pairs:
+                panel = armour_corners(pair)
+                icon_detection(panel, frame)
+                get_cord(panel)
+                panels.append(panel)
+                if DEBUG:
+                    draw(panel, frame)
+        except Exception as e:
+            print(f"Error in get_cord: {e}")
+            cords = []
+    else:
+        cords = []
+
+    if DEBUG:
+        # print(cords)
+
+        # time logging
+        cv.imshow("frame", frame)
+    end_time = time.time()
+    elapsed_time = (end_time - start_time) * 1000.0
+    print(f"elapsed time: {elapsed_time:.4f} ms")
+
     if demo.filter is not None and demo.latest_update_time is not None:
-        demo.update_plates([np.array([1,1,0],dtype=np.float64)])
+        demo.update_plates(panels)
         with demo.state_lock:
             prediction = demo.update_filter()
 
