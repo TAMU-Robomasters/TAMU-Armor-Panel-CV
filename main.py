9ee855371ca8311191975009a7d2951ae9be9af2
jai <jaipatel8@outlook.com>
2025-07-09 19:34:18 -0500

    integrated pf

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ pipeline = rs.pipeline()
 
 # Configure streams
 config = rs.config()
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 60)
+config.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 30)
 #config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
 # Start streaming
